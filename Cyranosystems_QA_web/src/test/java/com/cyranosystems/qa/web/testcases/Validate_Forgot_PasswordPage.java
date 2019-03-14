268c0daa68258c9d900f978be55df7886898c739
Gowdham <gowdham.test@gmail.com>
2019-03-14 16:16:08 +0530

    TestCase fr

---
+++ Cyranosystems_QA_web/src/test/java/com/cyranosystems/qa/web/testcases/Validate_Forgot_PasswordPage.java
@@ -0,0 +1,19 @@
+package com.cyranosystems.qa.web.testcases;
+
+import org.testng.annotations.Test;
+
+import com.cyranosystems.qa.web.testbase.Test_Baseclass;
+
+public class Validate_Forgot_PasswordPage extends Test_Baseclass {
+	
+	
+	//Forgot Password page > With valid credentials (Tapping on Hyper link in email page in Username page)
+	
+	@Test(enabled = true)
+	public void forgot_passoword_validcredential() {
+		
+		forgot_pwd.forgot_password_request(config.username());
+		
+	}
+
+}
