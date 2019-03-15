799ed9922874f15ba7d6c036240d674bd02b023f
jomy <jomy.tester@gmail.com>
2019-03-15 18:41:37 +0530

    clip

--- Cyranosystems_QA_web/src/main/java/com/cyranosystems/qa/web/pages/Produce_yourclips_page.java
+++ Cyranosystems_QA_web/src/main/java/com/cyranosystems/qa/web/pages/Produce_yourclips_page.java
@@ -6,28 +6,22 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class Produce_yourclips_page {
-	
-	
-	
-	
-	
-	@FindBy(how=How.XPATH, using="//span[contains(text(),'PRODUCE')]")
+
+	@FindBy(how = How.XPATH, using = "//span[contains(text(),'PRODUCE')]")
 	public static WebElement clickOnProduce_btn;
-	
-	@FindBy(how=How.XPATH, using="//li[@id='cust-admin-clips']")
+
+	@FindBy(how = How.XPATH, using = "//li[@id='cust-admin-clips']")
 	public static WebElement clickOnYourClips_btn;
-	
-	@FindBy(how=How.XPATH, using="//button[contains(text(),'Clip')]")
+
+	@FindBy(how = How.XPATH, using = "//button[contains(text(),'Clip')]")
 	public static WebElement clickOnAddClip_btn;
-	
-	
-	
+
 	public void create_clip() {
-		
+
 		clickOnProduce_btn.click();
 		clickOnYourClips_btn.click();
 		clickOnAddClip_btn.click();
-		
+
 	}
 
 }
