c995a07d71b6f32d553d64ebc2efbc46840a39ca
Gowdham <gowdham.test@gmail.com>
2019-03-15 13:50:13 +0530

    Frgt class frgt test case

--- Cyranosystems_QA_web/src/main/java/com/cyranosystems/qa/web/pages/ForgotPassword_page.java
+++ Cyranosystems_QA_web/src/main/java/com/cyranosystems/qa/web/pages/ForgotPassword_page.java
@@ -9,66 +9,61 @@ import org.openqa.selenium.support.How;
 public class ForgotPassword_page {
 
 	@FindBy(how = How.XPATH, using = "//a[contains(text(),'Click Here')]")
-	public static WebElement click_here; 
-	
-	@FindBy(how = How.NAME, using = "forgotpasswordtxt" )
+	public static WebElement click_here;
+
+	@FindBy(how = How.NAME, using = "forgotpasswordtxt")
 	public static WebElement forgotpasswordtxt;
-	
+
 	@FindBy(how = How.XPATH, using = "//button[@type='submit']")
 	public static WebElement request_reset_link_btn;
-	
-	@FindBy(how = How.XPATH, using="//span[@class='succtxt-cls']")
-	public static WebElement reset_reqest_msg;
-	
-	@FindBy(how = How.XPATH, using="//input[@placeholder='Check Any Inbox!']")
+
+	@FindBy(how = How.XPATH, using = "//span[@class='errortxt-cls']")
+	public static WebElement reset_reqest_error_msg;
+
+	@FindBy(how = How.XPATH, using = "//span[@class='succtxt-cls']")
+	public static WebElement reset_reqest_success_msg;
+
+	@FindBy(how = How.XPATH, using = "//input[@placeholder='Check Any Inbox!']")
 	public static WebElement registered_email;
-	
-	@FindBy(how = How.XPATH, using="//button[contains(text(),'Go!')]")
+
+	@FindBy(how = How.XPATH, using = "//button[contains(text(),'Go!')]")
 	public static WebElement mailinator_go;
-	
-	@FindBy(how = How.XPATH, using="//div[@class='table-responsive']//table//tbody//tr")
+
+	@FindBy(how = How.XPATH, using = "//div[@class='table-responsive']//table//tbody//tr")
 	public static List<WebElement> table_list;
-	
-	@FindBy(how = How.XPATH, using="//iframe[@id='msg_body']")
+
+	@FindBy(how = How.XPATH, using = "//iframe[@id='msg_body']")
 	public static WebElement frame_list;
-	
-	@FindBy(how = How.XPATH, using="//table[@class='main']//table[2]//tr//td//a")
+
+	@FindBy(how = How.XPATH, using = "//table[@class='main']//table[2]//tr//td//a")
 	public static WebElement subject;
-	
-	
-	@FindBy(how = How.XPATH, using="//a[contains(text(),'Reset')]")
+
+	@FindBy(how = How.XPATH, using = "//a[contains(text(),'Reset')]")
 	public static WebElement mail_reset;
-	
-	@FindBy(how = How.XPATH, using="//input[@name='currentpasswordtxt']")
+
+	@FindBy(how = How.XPATH, using = "//input[@name='currentpasswordtxt']")
 	public static WebElement currentpasswordtxt;
-	
-	@FindBy(how = How.XPATH, using="//input[@name='newpasswordtxt']")
+
+	@FindBy(how = How.XPATH, using = "//input[@name='newpasswordtxt']")
 	public static WebElement newpasswordtxt;
-	
-	@FindBy(how = How.XPATH, using="//input[@name='confirmpasswordtxt']")
+
+	@FindBy(how = How.XPATH, using = "//input[@name='confirmpasswordtxt']")
 	public static WebElement confirmpasswordtxt;
-	
-	@FindBy(how = How.XPATH, using="//button[@type='submit']")
+
+	@FindBy(how = How.XPATH, using = "//button[@type='submit']")
 	public static WebElement submit_reset_password;
-	
-	@FindBy(how = How.XPATH, using="//div[@class='d-inline-block']//button")
+
+	@FindBy(how = How.XPATH, using = "//div[@class='d-inline-block']//button")
 	public static WebElement finish;
-	
-	
-	
-public void forgot_password_request(String username) {
-		
+
+	public void forgot_password_request(String username) {
+
 		click_here.click();
-		
+
 		forgotpasswordtxt.sendKeys(username);
-		
+
 		request_reset_link_btn.click();
-		
-		
+
 	}
 
-
-
-
-	
 }
--- Cyranosystems_QA_web/src/test/java/com/cyranosystems/qa/web/testcases/Validate_ForgotPassword.java
+++ Cyranosystems_QA_web/src/test/java/com/cyranosystems/qa/web/testcases/Validate_ForgotPassword.java
@@ -1,9 +1,8 @@
 package com.cyranosystems.qa.web.testcases;
 
-
 import java.util.Set;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -13,43 +12,43 @@ import com.cyranosystems.qa.web.pages.Login_page;
 import com.cyranosystems.qa.web.testbase.Test_Baseclass;
 
 public class Validate_ForgotPassword extends Test_Baseclass {
-	
-	//Forgot Password page > With valid credentials (Clicking on Hyper link in email page in Username page)
-	
-	@Test(enabled = true)
-	public void forgotPassoword_usernamePage_validcredential() {
-		
+
+	// Forgot Password page > With valid credentials (Clicking on Hyper link in
+	// email page in Username page)
+
+	@Test(enabled = false)
+	public void forgotPassoword_usernamePage_validCredential() {
+
 		forgot_pwd.forgot_password_request(config.username_frgt_pwd());
-		String error_msg = forgot_pwd.reset_reqest_msg.getText();
-		Assert.assertEquals(error_msg,
-				"Temporary password will be sent to the below mentioned email address");
-		
+		String error_msg = forgot_pwd.reset_reqest_success_msg.getText();
+		Assert.assertEquals(error_msg, "Temporary password will be sent to the below mentioned email address");
+
 		// Navigate to Mailinator
 		browser_factory.navigate_to(driver, config.mailinator_url());
 		ForgotPassword_page.registered_email.sendKeys(config.username_frgt_pwd());
 		ForgotPassword_page.mailinator_go.click();
-		
-		for(int i=0; i<ForgotPassword_page.table_list.size(); i++) {
-			
-			if(ForgotPassword_page.table_list.get(i).getText().contains("Forget something")) {
-				//System.out.println(ForgotPassword_page.table_list.get(i).getText());
+
+		for (int i = 0; i < ForgotPassword_page.table_list.size(); i++) {
+
+			if (ForgotPassword_page.table_list.get(i).getText().contains("Forget something")) {
+				// System.out.println(ForgotPassword_page.table_list.get(i).getText());
 				ForgotPassword_page.table_list.get(i).click();
-				
-			}else {
+
+			} else {
 				System.out.println("nothing");
 			}
 		}
-		
+
 		WebElement ele = ForgotPassword_page.frame_list;
 		driver.switchTo().frame(ele);
-		
+
 		String str = ForgotPassword_page.subject.getText();
 		System.out.println(str);
-				
+
 		String parentWindow = driver.getWindowHandle();
-		
+
 		ForgotPassword_page.mail_reset.click();
-		
+
 		Set<String> allwinodws = driver.getWindowHandles();
 
 		for (String childWindows : allwinodws) {
@@ -62,52 +61,52 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 					System.out.println(driver.getTitle());
 
 				}
-			}			
+			}
 		}
-		
+
 		ForgotPassword_page.newpasswordtxt.sendKeys(config.password());
 		ForgotPassword_page.confirmpasswordtxt.sendKeys(config.password());
 		ForgotPassword_page.submit_reset_password.click();
 		
+
 	}
-	
-	@Test(enabled = true)
-	public void forgotPassoword_passwrodPage_validcredential_() {
-		
+
+	@Test(enabled = false)
+	public void forgotPassoword_passwrodPage_validCredential() {
+
 		Login_page.username_txt.sendKeys(config.username_frgt_pwd());
 		Login_page.clickon_next.click();
-		
+
 		forgot_pwd.forgot_password_request(config.username_frgt_pwd());
-		String error_msg = forgot_pwd.reset_reqest_msg.getText();
-		Assert.assertEquals(error_msg,
-				"Temporary password will be sent to the below mentioned email address");
-		
+		String error_msg = forgot_pwd.reset_reqest_success_msg.getText();
+		Assert.assertEquals(error_msg, "Temporary password will be sent to the below mentioned email address");
+
 		// Navigate to Mailinator
 		browser_factory.navigate_to(driver, config.mailinator_url());
 		ForgotPassword_page.registered_email.sendKeys(config.username_frgt_pwd());
 		ForgotPassword_page.mailinator_go.click();
-		
-		for(int i=0; i<ForgotPassword_page.table_list.size(); i++) {
-			
-			if(ForgotPassword_page.table_list.get(i).getText().contains("Forget something")) {
-				//System.out.println(ForgotPassword_page.table_list.get(i).getText());
+
+		for (int i = 0; i < ForgotPassword_page.table_list.size(); i++) {
+
+			if (ForgotPassword_page.table_list.get(i).getText().contains("Forget something")) {
+				// System.out.println(ForgotPassword_page.table_list.get(i).getText());
 				ForgotPassword_page.table_list.get(i).click();
-				
-			}else {
+
+			} else {
 				System.out.println("nothing");
 			}
 		}
-		
+
 		WebElement ele = ForgotPassword_page.frame_list;
 		driver.switchTo().frame(ele);
-		
+
 		String str = ForgotPassword_page.subject.getText();
 		System.out.println(str);
-				
+
 		String parentWindow = driver.getWindowHandle();
-		
+
 		ForgotPassword_page.mail_reset.click();
-		
+
 		Set<String> allwinodws = driver.getWindowHandles();
 
 		for (String childWindows : allwinodws) {
@@ -120,15 +119,68 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 					System.out.println(driver.getTitle());
 
 				}
-			}			
+			}
 		}
-		
+
 		ForgotPassword_page.newpasswordtxt.sendKeys(config.password());
 		ForgotPassword_page.confirmpasswordtxt.sendKeys(config.password());
 		ForgotPassword_page.submit_reset_password.click();
-		
+
 	}
+
+	@Test(enabled = false)
+	public void forgotPassoword_invalidemail() {
+
+		Login_page.username_txt.sendKeys(config.username_frgt_pwd());
+		Login_page.clickon_next.click();
+		String error_msg = forgot_pwd.reset_reqest_error_msg.getText();
+		Assert.assertEquals(error_msg, "User does not exist!");
+
+	}
+	@Test(enabled = true)
+	public void forgotPassword_currentPassword_Manual() {
+		
+		forgot_pwd.forgot_password_request(config.username_frgt_pwd());
+		String error_msg = forgot_pwd.reset_reqest_success_msg.getText();
+		Assert.assertEquals(error_msg, "Temporary password will be sent to the below mentioned email address");
+
+		// Navigate to Mailinator
+		browser_factory.navigate_to(driver, config.mailinator_url());
+		ForgotPassword_page.registered_email.sendKeys(config.username_frgt_pwd());
+		ForgotPassword_page.mailinator_go.click();
+
+		for (int i = 0; i < ForgotPassword_page.table_list.size(); i++) {
+
+			if (ForgotPassword_page.table_list.get(i).getText().contains("Forget something")) {
+				// System.out.println(ForgotPassword_page.table_list.get(i).getText());
+				ForgotPassword_page.table_list.get(i).click();
+
+			} else {
+				System.out.println("nothing");
+			}
+		}
+
+		WebElement ele = ForgotPassword_page.frame_list;
+		driver.switchTo().frame(ele);
+
+		String str = ForgotPassword_page.subject.getText();
+		System.out.println(str);
+		
+		// Cyrano page 
+		browser_factory.navigate_to(driver, config.staging_url());
+		login.valid_login(config.username(), str);
+		ForgotPassword_page.currentpasswordtxt.sendKeys(str);
+		ForgotPassword_page.newpasswordtxt.sendKeys(config.password());
+		ForgotPassword_page.confirmpasswordtxt.sendKeys(config.password());
+		
+		
+		
+		
+		
+		
 		
 	}
 	
 	
+	
+}
