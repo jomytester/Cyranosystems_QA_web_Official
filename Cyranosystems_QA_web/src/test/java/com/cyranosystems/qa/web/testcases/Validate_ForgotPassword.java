07dcb2052c95408561cd130772f88cd3e22cc0dd
jomy <jomy.tester@gmail.com>
2019-03-19 15:15:53 +0530

    just update

--- Cyranosystems_QA_web/src/test/java/com/cyranosystems/qa/web/testcases/Validate_ForgotPassword.java
+++ Cyranosystems_QA_web/src/test/java/com/cyranosystems/qa/web/testcases/Validate_ForgotPassword.java
@@ -1,5 +1,6 @@
 package com.cyranosystems.qa.web.testcases;
 
+
 import java.util.Set;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -8,6 +9,7 @@ import com.cyranosystems.qa.web.pages.ForgotPassword_page;
 import com.cyranosystems.qa.web.pages.Login_page;
 import com.cyranosystems.qa.web.testbase.Test_Baseclass;
 import com.cyranosystems.qa.web.utilities.Browser_factory;
+
 
 public class Validate_ForgotPassword extends Test_Baseclass {
 
@@ -37,8 +39,32 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 		ForgotPassword_page.mail_reset.click();
 
-	//	forgot_pwd.redirect_to_cyrano();
-		
+		String parentWindow = driver.getWindowHandle();
+
+		Set<String> allwinodws = driver.getWindowHandles();
+
+		for (String childWindows : allwinodws) {
+
+			if (!childWindows.equals(parentWindow)) {
+
+				driver.switchTo().window(childWindows);
+
+				String actualTitle = driver.getTitle();
+
+				System.out.println(driver.getTitle());
+
+				if (actualTitle.equalsIgnoreCase("Cyrano")) {
+
+				} else {
+
+					System.out.println("error in handling window");
+
+				}
+
+			}
+
+		}
+
 		ForgotPassword_page.newpasswordtxt.sendKeys(config.password());
 
 		ForgotPassword_page.confirmpasswordtxt.sendKeys(config.password());
@@ -55,7 +81,7 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 	}
 
-	@Test(enabled = false, priority = 2)
+	@Test(enabled = true, priority = 2)
 	public void forgotPassoword_passwrodPage_validCredential() throws InterruptedException {
 
 		Login_page.username_txt.sendKeys(config.username_frgt_pwd());
@@ -69,7 +95,7 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		Assert.assertEquals(msg, "Temporary password will be sent to the below mentioned email address");
 
 		// Navigate to Mailinator
-
+		
 		helper.redirecting_to_mailinator();
 
 		WebElement ele = ForgotPassword_page.frame_list;
@@ -84,7 +110,31 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 		// Redirecting to Cyrano
 
-	//	forgot_pwd.redirect_to_cyrano();
+		String parentWindow = driver.getWindowHandle();
+
+		Set<String> allwinodws = driver.getWindowHandles();
+
+		for (String childWindows : allwinodws) {
+
+			if (!childWindows.equals(parentWindow)) {
+
+				driver.switchTo().window(childWindows);
+
+				String actualTitle = driver.getTitle();
+
+				System.out.println(driver.getTitle());
+
+				if (actualTitle.equalsIgnoreCase("Cyrano")) {
+
+				} else {
+
+					System.out.println("error in handling window");
+
+				}
+
+			}
+
+		}
 
 		ForgotPassword_page.newpasswordtxt.sendKeys(config.password());
 
@@ -102,7 +152,7 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 	}
 
-	@Test(enabled = false, priority = 3)
+	@Test(enabled = true, priority = 3)
 	public void forgotPassword_currentPassword_Manual() throws InterruptedException {
 
 		forgot_pwd.forgot_password_request(config.username_frgt_pwd());
@@ -112,7 +162,7 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		Assert.assertEquals(msg, "Temporary password will be sent to the below mentioned email address");
 
 		// Navigate to Mailinator
-
+		
 		helper.redirecting_to_mailinator();
 
 		WebElement ele = ForgotPassword_page.frame_list;
@@ -125,9 +175,13 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 		// Redirecting to Cyrano
 
+		browser_factory.navigate_to(config.staging_url());
 
+		login.valid_login(config.username(), str);
+
+		
 		Browser_factory.navigate_to(config.staging_url());
-
+		
 		login.valid_login(config.username_frgt_pwd(), str);
 
 
@@ -151,9 +205,9 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 	}
 
-	@Test(enabled = false, priority = 4)
+	@Test(enabled = true, priority = 4)
 	public void forgotPassoword_invalidemail() {
-
+		
 		forgot_pwd.forgot_password_request("auto123@gmail.com");
 
 
@@ -168,8 +222,7 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 	}
 
-
-	@Test(enabled = false, priority = 5)
+	@Test(enabled = true, priority = 5)
 
 	public void forgotPassword_invalidCurrentPassword_passwordPage() throws InterruptedException {
 
@@ -181,7 +234,7 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		Assert.assertEquals(msg, "Temporary password will be sent to the below mentioned email address");
 
 		// Navigate to Mailinator
-
+		
 		helper.redirecting_to_mailinator();
 
 		WebElement ele = ForgotPassword_page.frame_list;
@@ -201,9 +254,30 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		
 		String parentWindow = driver.getWindowHandle();
 
+		Set<String> allwinodws = driver.getWindowHandles();
 
-	//	forgot_pwd.redirect_to_cyrano();
+		for (String childWindows : allwinodws) {
 
+			if (!childWindows.equals(parentWindow)) {
+
+				driver.switchTo().window(childWindows);
+
+				String actualTitle = driver.getTitle();
+
+				System.out.println(driver.getTitle());
+
+				if (actualTitle.equalsIgnoreCase("Cyrano")) {
+
+				} else {
+
+					System.out.println("error in handling window");
+
+				}
+
+			}
+
+		}
+		
 		Browser_factory.navigate_to(config.staging_url());
 
 		login.valid_login(config.username_frgt_pwd(), "300100");
@@ -217,8 +291,8 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		}
 	}
 
+	@Test(enabled = true, priority = 6)
 
-	@Test(enabled = false, priority = 6)
 	public void forgotPassword_invalidCurrentPassword_resetPage() throws InterruptedException {
 
 
@@ -229,7 +303,7 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		Assert.assertEquals(msg, "Temporary password will be sent to the below mentioned email address");
 
 		// Navigate to Mailinator
-
+		
 		helper.redirecting_to_mailinator();
 
 		WebElement ele = ForgotPassword_page.frame_list;
@@ -242,7 +316,6 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 		// Redirecting to Cyrano
 
-
 		helper.redirecting_to_mailinator();
 
 		login.valid_login(config.username(), str);
@@ -250,11 +323,32 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		
 		String parentWindow = driver.getWindowHandle();
 
+		Set<String> allwinodws = driver.getWindowHandles();
 
-//		forgot_pwd.redirect_to_cyrano();
+		for (String childWindows : allwinodws) {
 
+			if (!childWindows.equals(parentWindow)) {
+
+				driver.switchTo().window(childWindows);
+
+				String actualTitle = driver.getTitle();
+
+				System.out.println(driver.getTitle());
+
+				if (actualTitle.equalsIgnoreCase("Cyrano")) {
+
+				} else {
+
+					System.out.println("error in handling window");
+
+				}
+
+			}
+
+		}
+		
 		Browser_factory.navigate_to(config.staging_url());
-
+		
 		login.valid_login(config.username_frgt_pwd(), str);
 
 
@@ -272,8 +366,9 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 	}
 
+	@Test(enabled = true, priority = 7)
 
-	@Test(enabled = false, priority = 7)
+
 
 	public void forgotPassword_different_newPassword_confirmPassword() throws InterruptedException {
 
@@ -286,7 +381,7 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		Assert.assertEquals(msg, "Temporary password will be sent to the below mentioned email address");
 
 		// Navigate to Mailinator
-
+		
 		helper.redirecting_to_mailinator();
 
 		WebElement ele = ForgotPassword_page.frame_list;
@@ -296,13 +391,37 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		String str = ForgotPassword_page.subject.getText();
 
 		System.out.println(str);
-
+		
 		ForgotPassword_page.mail_reset.click();
 
 		// Redirecting to Cyrano
-
-		//forgot_pwd.redirect_to_cyrano();
 		
+		String parentWindow = driver.getWindowHandle();
+
+		Set<String> allwinodws = driver.getWindowHandles();
+
+		for (String childWindows : allwinodws) {
+
+			if (!childWindows.equals(parentWindow)) {
+
+				driver.switchTo().window(childWindows);
+
+				String actualTitle = driver.getTitle();
+
+				System.out.println(driver.getTitle());
+
+				if (actualTitle.equalsIgnoreCase("Cyrano")) {
+
+				} else {
+
+					System.out.println("error in handling window");
+
+				}
+
+			}
+
+		}
+
 		ForgotPassword_page.newpasswordtxt.sendKeys("Test@2222");
 
 		ForgotPassword_page.confirmpasswordtxt.sendKeys("Test@7777");
@@ -315,7 +434,7 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 	}
 
-	@Test(enabled = false, priority = 8)
+	@Test(enabled = true, priority = 8)
 	public void forgotPassword_invalid_newPassword_confirmPassword() throws InterruptedException {
 
 		forgot_pwd.forgot_password_request(config.username_frgt_pwd());
@@ -326,7 +445,6 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		Assert.assertEquals(msg, "Temporary password will be sent to the below mentioned email address");
 
 		// Navigate to Mailinator
-
 		helper.redirecting_to_mailinator();
 
 		WebElement ele = ForgotPassword_page.frame_list;
@@ -336,7 +454,7 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		String str = ForgotPassword_page.subject.getText();
 
 		System.out.println(str);
-
+		
 		ForgotPassword_page.mail_reset.click();
 
 		// Redirecting to Cyrano
@@ -349,8 +467,29 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 		String parentWindow = driver.getWindowHandle();
 
+		Set<String> allwinodws = driver.getWindowHandles();
 
-		//forgot_pwd.redirect_to_cyrano();
+		for (String childWindows : allwinodws) {
+
+			if (!childWindows.equals(parentWindow)) {
+
+				driver.switchTo().window(childWindows);
+
+				String actualTitle = driver.getTitle();
+
+				System.out.println(driver.getTitle());
+
+				if (actualTitle.equalsIgnoreCase("Cyrano")) {
+
+				} else {
+
+					System.out.println("error in handling window");
+
+				}
+
+			}
+
+		}
 
 		ForgotPassword_page.newpasswordtxt.sendKeys("Test2345");
 
@@ -370,8 +509,10 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 	}
 
 
-	@Test(enabled = false, priority = 9)
+	
 
+	
+	@Test(enabled = true, priority = 9)
 	public void forgotPassword_invalid_newPassword() throws InterruptedException {
 
 
@@ -382,7 +523,6 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		Assert.assertEquals(msg, "Temporary password will be sent to the below mentioned email address");
 
 		// Navigate to Mailinator
-
 		helper.redirecting_to_mailinator();
 
 		WebElement ele = ForgotPassword_page.frame_list;
@@ -392,11 +532,10 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		String str = ForgotPassword_page.subject.getText();
 
 		System.out.println(str);
-
+		
 		ForgotPassword_page.mail_reset.click();
 
 		// Redirecting to Cyrano
-
 
 		helper.redirecting_to_mailinator();
 
@@ -407,8 +546,29 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		
 		String parentWindow = driver.getWindowHandle();
 
+		Set<String> allwinodws = driver.getWindowHandles();
 
-	//	forgot_pwd.redirect_to_cyrano();
+		for (String childWindows : allwinodws) {
+
+			if (!childWindows.equals(parentWindow)) {
+
+				driver.switchTo().window(childWindows);
+
+				String actualTitle = driver.getTitle();
+
+				System.out.println(driver.getTitle());
+
+				if (actualTitle.equalsIgnoreCase("Cyrano")) {
+
+				} else {
+
+					System.out.println("error in handling window");
+
+				}
+
+			}
+
+		}
 
 		ForgotPassword_page.newpasswordtxt.sendKeys("Test2345test");
 
@@ -427,9 +587,8 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 
 	}
 
-
-	@Test(enabled = false, priority = 10)
-
+	
+	@Test(enabled = true, priority = 10)
 	public void forgotPassword_invalid_currentPassword() throws InterruptedException {
 
 		forgot_pwd.forgot_password_request(config.username_frgt_pwd());
@@ -439,7 +598,6 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		Assert.assertEquals(msg, "Temporary password will be sent to the below mentioned email address");
 
 		// Navigate to Mailinator
-
 		helper.redirecting_to_mailinator();
 
 		WebElement ele = ForgotPassword_page.frame_list;
@@ -449,13 +607,37 @@ public class Validate_ForgotPassword extends Test_Baseclass {
 		String str = ForgotPassword_page.subject.getText();
 
 		System.out.println(str);
-
+		
 		ForgotPassword_page.mail_reset.click();
 
 		// Redirecting to Cyrano
-
-	//	forgot_pwd.redirect_to_cyrano();
 		
+		String parentWindow = driver.getWindowHandle();
+
+		Set<String> allwinodws = driver.getWindowHandles();
+
+		for (String childWindows : allwinodws) {
+
+			if (!childWindows.equals(parentWindow)) {
+
+				driver.switchTo().window(childWindows);
+
+				String actualTitle = driver.getTitle();
+
+				System.out.println(driver.getTitle());
+
+				if (actualTitle.equalsIgnoreCase("Cyrano")) {
+
+				} else {
+
+					System.out.println("error in handling window");
+
+				}
+
+			}
+
+		}
+
 		ForgotPassword_page.newpasswordtxt.sendKeys(config.password());
 
 		ForgotPassword_page.confirmpasswordtxt.sendKeys("Test2345567");
