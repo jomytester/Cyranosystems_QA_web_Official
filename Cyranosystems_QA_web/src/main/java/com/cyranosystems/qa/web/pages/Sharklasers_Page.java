99d88cb57d58284a4ab2fdcd483a872c51821d9b
Gowdham <gowdham.test@gmail.com>
2019-04-09 11:57:25 +0530

    Sharklasers page

---
+++ Cyranosystems_QA_web/src/main/java/com/cyranosystems/qa/web/pages/Sharklasers_Page.java
@@ -0,0 +1,45 @@
+package com.cyranosystems.qa.web.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class Sharklasers_Page {
+
+	WebDriver driver;
+	
+	@FindBy(how = How.XPATH, using = "//span[@id='inbox-id']")
+	public static WebElement enter_email;
+	
+	@FindBy(how = How.XPATH, using = "//button[@class='save button small']")
+	public static WebElement enter_email_setButton;
+	
+	@FindBy(how = How.XPATH, using = "//span//input[@type='text']")
+	public static WebElement enter_email_textBox;
+	
+	@FindBy(how = How.XPATH, using = "")
+	public static WebElement m3;
+	
+	@FindBy(how = How.XPATH, using = "")
+	public static WebElement m55;
+	
+	@FindBy(how = How.XPATH, using = "")
+	public static WebElement m5;
+	
+	@FindBy(how = How.XPATH, using = "")
+	public static WebElement m6;
+	
+	@FindBy(how = How.XPATH, using = "")
+	public static WebElement m7;
+	
+	@FindBy(how = How.XPATH, using = "")
+	public static WebElement m8;
+	
+	public Sharklasers_Page(WebDriver driver) {
+
+		this.driver = driver;
+	}
+	
+	
+}
